8aa5393b3e317f7a0c7085b84dd391555cea3441
Michael Brunner <michael.brunner@sm0x.org>
2025-01-14 21:06:36 +0100

    basic chat menu added

--- frontend/src/Content/Chat/Chat.tsx
+++ frontend/src/Content/Chat/Chat.tsx
@@ -1,14 +1,44 @@
+import { Avatar, Typography } from '@mui/material';
 import './Chat.css';
 
 
-export interface IChatProps {
-  
-}
+const Chat = () => {
 
-const Chat = (props: IChatProps) => {
+  const friends = [{
+      name: "Marius Hofer",
+      avatar: '/avatar/mh_profilbid.jpg'
+    }, {
+      name: "Stefan Kowalski",
+      avatar: '/avatar/sk_profilbild.jpg'
+    }, {
+      name: "Lena Meier",
+      avatar: '/avatar/lm_profilbild.jpg'
+    }, {
+      name: "Maximilian Schwarzmüller",
+      avatar: '/avatar/ms_profilbild.jpg'
+    },{
+      name: "Sandra Müller",
+      avatar: '/avatar/sm_profilbild.jpg'
+    }, {
+      name: "Max Mustermann",
+      avatar: '/avatar/mm_profilbild.jpg'
+    }
+  ];
+
+
   return (
     <div className="chat-wrapper">
-      Chat
+      <div className="chat">
+        <Typography sx={{ textAlign: "center", marginTop: "20px" }} className="chat-title" variant="h4">Chat</Typography>
+        <ul className="user-list">
+          { friends.map(friend => (
+            <div className="user-list-item">
+              <Avatar className='user-list__avatar' src={friend.avatar} />
+              <Typography className='user-list__name' variant="h6">{friend.name}</Typography>
+            </div>
+          )) }
+        </ul>
+      </div>
     </div>
   );
 }
